f9f713cd4d3b930c66101304e15522f69e7fa4b1
KarimAshraf1995 <karimashraf1995@gmail.com>
2017-03-14 10:49:11 +0200

    Update SpiderWeb
    Faster AJAX Suggestions response by create index on sugguestion column
    (space tradeoff)

--- SpiderWeb/ajax/suggestions.php
+++ SpiderWeb/ajax/suggestions.php
@@ -13,7 +13,7 @@ if(strlen($q)>2){
 	$db = new SQLite3(strtoupper($q[0]).'.db');
 
 
-	$results = $db->query("SELECT DISTINCT Suggestion FROM Suggestions WHERE Suggestion like '".$q."%' ORDER BY Suggestion LIMIT 50");
+	$results = $db->query("SELECT DISTINCT Suggestion FROM Suggestions WHERE Suggestion like '".$q."%' ORDER BY Rank LIMIT 50");
 	while ($row = $results->fetchArray()) {
 		$rows[] = $row["Suggestion"];
 	}
