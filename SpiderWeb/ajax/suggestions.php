a4cf5f334662ba00b3e48f67a2e282c823a7a815
KarimAshraf1995 <karimashraf1995@gmail.com>
2017-04-13 14:31:35 +0200

    Fix suggestions.php

--- SpiderWeb/ajax/suggestions.php
+++ SpiderWeb/ajax/suggestions.php
@@ -1,5 +1,4 @@
 <?php 
-
 if(!isset($_GET["q"]))
 {die();}
 
@@ -8,17 +7,16 @@ $q=$_GET["q"];
 $rows = array();
 $results = null;
 
+
 if(strlen($q)>2){
+	if(($q[0]<='Z' and $q[0]>='A') OR ($q[0]<='z' and $q[0]>='a')){
+		$db = new SQLite3(strtoupper($q[0]).'.db');
 
-	$db = new SQLite3(strtoupper($q[0]).'.db');
-
-
-	$results = $db->query("SELECT DISTINCT Suggestion FROM Suggestions WHERE Suggestion like '".$q."%' ORDER BY Rank LIMIT 50");
-	while ($row = $results->fetchArray()) {
-		$rows[] = $row["Suggestion"];
+		$results = $db->query("SELECT DISTINCT Suggestion FROM Suggestions WHERE Suggestion like '".$q."%' ORDER BY Rank LIMIT 50");
+		while ($row = $results->fetchArray()) {
+			$rows[] = $row["Suggestion"];
+		}
 	}
-
 }
 echo json_encode($rows);
-
 ?>
