aa6768866f4c1bef69d18735c5da7a0cb838bb15
KarimAshraf1995 <karimashraf1995@gmail.com>
2017-05-16 22:06:42 +0200

    Merged Salah Image Search and made it functional

--- Spider/Spider/DBController.cs
+++ Spider/Spider/DBController.cs
@@ -87,15 +87,17 @@ namespace Spider
             sql = "CREATE VIRTUAL TABLE IF NOT EXISTS PageContent USING fts4 (LID REFERENCES URL (ID) ON DELETE CASCADE ON UPDATE CASCADE, Content TEXT,tokenize=porter);";
 
             database.ExecuteNonQuery(sql);
-            /*
+            
             sql = "CREATE VIRTUAL TABLE IF NOT EXISTS Images USING fts4 (LID REFERENCES URL (ID) ON DELETE CASCADE ON UPDATE CASCADE, ImageLink VARCHAR(150), ImageAlt TEXT);";
 
             database.ExecuteNonQuery(sql);
-
+            
+            /*
             sql = "CREATE VIRTUAL TABLE IF NOT EXISTS StepsSuggestions USING fts4 (LID REFERENCES URL (ID) ON DELETE CASCADE ON UPDATE CASCADE, Header TEXT, List VARCHAR(150));";
 
             database.ExecuteNonQuery(sql);
             */
+
             sql = "PRAGMA synchronous = 0";
 
             database.ExecuteNonQuery(sql);
